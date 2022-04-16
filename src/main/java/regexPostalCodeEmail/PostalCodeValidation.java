97c45cebbc1346049809d5654eb9f76c01c4c9b2
unknown <mohit16gaikwad@gmail.com>
2022-04-16 11:03:18 +0530

    Added: code to validate pincode

--- src/main/java/regexPostalCodeEmail/PostalCodeValidation.java
+++ src/main/java/regexPostalCodeEmail/PostalCodeValidation.java
@@ -8,9 +8,9 @@ public class PostalCodeValidation {
 		public static void main(String[] args) {
 	      
 			System.out.println("Welcome to Java regex program");
-			String postalPattern = "400088B"; // string to be validated
-			String matcher = "[1-9]{1}[0-9]{5}$"; // regular expression to restrict character at end and begining
-			System.out.println(Pattern.matches(matcher, postalPattern));//prints false
+			String postalPattern = "400 088"; // string to be validated
+			String matcher = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$"; 
+			System.out.println(Pattern.matches(matcher, postalPattern));
 	}
 
 }
