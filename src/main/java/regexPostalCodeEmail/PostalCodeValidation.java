48cae544aa5d0272138790243c85d0c4eb317fd7
unknown <mohit16gaikwad@gmail.com>
2022-04-16 10:55:41 +0530

    Added: code to restrict character at end

--- src/main/java/regexPostalCodeEmail/PostalCodeValidation.java
+++ src/main/java/regexPostalCodeEmail/PostalCodeValidation.java
@@ -8,9 +8,9 @@ public class PostalCodeValidation {
 		public static void main(String[] args) {
 	      
 			System.out.println("Welcome to Java regex program");
-			String postalPattern = "A67895"; 
-			String matcher = "[1-9]{1}[0-9]{5}"; 
-			System.out.println(Pattern.matches(matcher,postalPattern));
+			String postalPattern = "400088B"; // string to be validated
+			String matcher = "[1-9]{1}[0-9]{5}$"; // regular expression to restrict character at end and begining
+			System.out.println(Pattern.matches(matcher, postalPattern));//prints false
 	}
 
 }
