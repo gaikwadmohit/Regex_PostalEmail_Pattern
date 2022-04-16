97645447040acfbb0eac6d76a5aa5986064950f5
unknown <mohit16gaikwad@gmail.com>
2022-04-16 10:46:55 +0530

    Added: codde to validate 6 digit pincode

--- src/main/java/regexPostalCodeEmail/PostalCodeValidation.java
+++ src/main/java/regexPostalCodeEmail/PostalCodeValidation.java
@@ -8,10 +8,9 @@ public class PostalCodeValidation {
 		public static void main(String[] args) {
 	      
 			System.out.println("Welcome to Java regex program");
-			String postalPattern = "400088"; //postal code to be validated
-			String matcher = "[1-9]{1}[0-9]{5}"; //matching
+			String postalPattern = "400088"; //string to be validated
+			String matcher = "[1-9]{1}[0-9]{5}"; //regular expression
 			System.out.println(Pattern.matches(matcher,postalPattern));
-		}
-
 	}
 
+}
