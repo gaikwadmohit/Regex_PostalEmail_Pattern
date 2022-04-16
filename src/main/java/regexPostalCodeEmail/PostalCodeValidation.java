fc5c548ea51654535aeb73a569aab568a724bed9
unknown <mohit16gaikwad@gmail.com>
2022-04-16 10:52:37 +0530

    Added: code to restrict character at beginning

--- src/main/java/regexPostalCodeEmail/PostalCodeValidation.java
+++ src/main/java/regexPostalCodeEmail/PostalCodeValidation.java
@@ -8,8 +8,8 @@ public class PostalCodeValidation {
 		public static void main(String[] args) {
 	      
 			System.out.println("Welcome to Java regex program");
-			String postalPattern = "400088"; //string to be validated
-			String matcher = "[1-9]{1}[0-9]{5}"; //regular expression
+			String postalPattern = "A67895"; 
+			String matcher = "[1-9]{1}[0-9]{5}"; 
 			System.out.println(Pattern.matches(matcher,postalPattern));
 	}
 
